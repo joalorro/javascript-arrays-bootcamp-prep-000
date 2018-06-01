69c1206f0a84eacf4acd69eb0d8d6fb4f73bb602
Josue V. Alorro <joalorro@gmail.com>
2018-06-01 16:18:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -57,7 +57,7 @@ function destructivelyRemoveElementFromEndOfArray(arr) {
 
 function removeElementFromEndOfArray(arr) {
   
-  var newArr = arr.slice()
+  var newArr = arr.slice(0, arr.length - 1)
   return newArr
   
 }
